5ba0fa2ac2676018a7dfacedced7547ba8be8de2
Nicholas Miller <nickbryanmiller@gmail.com>
2016-03-21 16:36:27 -0400

    Added the header with my name and information about the program

--- Part 7/hw7/application.linux-armv6hf/source/hw7.java
+++ Part 7/hw7/application.linux-armv6hf/source/hw7.java
@@ -14,6 +14,24 @@ import java.io.IOException;
 
 public class hw7 extends PApplet {
 
+/*
+Nicholas Miller
+This is Homework 7!
+It is a desktop application that helps students improve their typing skills.
+The 'hw7' class is the main file. Everything stems from here.
+The 'Menu' class controls all of the menu buttons and appearance.
+The 'Levels' class controls each of the levels: Levelx (where x is the level number).
+This is useful because we can copy code over from any level class to a new level class.
+The only thing you have to change is the list of data items and then add this to
+the 'Levels' class and then it is good to go and runs with another level.
+The 'MyColors' class is a utility to grab any color easily.
+The 'Score' class keeps the score and displays it.
+The 'Button' class uses rectangles, strings, and math to create a button.
+This is a highly useful class because the button has a 'isClicked' method that returns
+if the button what clicked by the mouse.
+We uses 'hw7' to send all the mouse location information and reset information
+down the line to all the other classes.
+*/
 
 MyColors c = new MyColors();
 Menu menu = new Menu();
